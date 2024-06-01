132c151286d5217bb3bd2c2888ce9dacf5d16949
Tushar Sahani <tusharsahani000@gmail.com>
2024-06-01 12:00:29 +0530

    assignment1

--- assignment2/src/App.js
+++ assignment2/src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import axios from 'axios';
 import ProductList from './components/ProductList';
 import ProductPage from './components/ProductPage';
@@ -24,16 +24,10 @@ const App = () => {
   return (
     <Router>
       <div className="container mx-auto px-4 py-8">
-        <Routes> 
-          <Route exact path="/">
-            <>
-            <h1 className="text-3xl font-semibold mb-4">Top Products</h1>
-            <ProductList products={products} />
-            </>
-          </Route>
-          <Route path="/product/:productId">
-            <ProductPage />
-          </Route>
+        <Routes>
+          <Route exact path="/" element={<h1 className="text-3xl font-semibold mb-4">Top Products</h1>} />
+          <Route exact path="/" element={<ProductList products={products} />} />
+          <Route path="/product/:productId" element={<ProductPage />} />
         </Routes>
       </div>
     </Router>
